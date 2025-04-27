d114051856d8c29966182a510f7279142c43172b
TeamPlasticure <TeamPlastiCure@gmail.com>
2025-04-27 21:00:20 +0400

    script.js

--- script.js
+++ script.js
@@ -111,3 +111,31 @@ document.getElementById('submit-btn').onclick = async () => {
     nameInput.value = "";
   }, 'image/png');
 };
+
+// --- Countdown Timer Logic ---
+(function() {
+  // Set graduation date (June 27, 2025, 00:00:00 local time)
+  var countDownDate = new Date("June 27, 2025 00:00:00").getTime();
+
+  function updateCountdown() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    if (distance < 0) {
+      document.getElementById("countdown-timer").innerHTML = "🎓 Congratulations, Year 11!";
+      clearInterval(timerInterval);
+      return;
+    }
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown-timer").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }
+
+  updateCountdown(); // initial call
+  var timerInterval = setInterval(updateCountdown, 1000);
+})();
